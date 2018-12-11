1f91e267183c943ea5a8db614fe0a0b1fbc4e071
ArenChenQiwei <38103251+ArenChenQiwei@users.noreply.github.com>
2018-12-11 09:37:44 +0800

    Add files via upload

---
+++ chat_server.py
@@ -0,0 +1,289 @@
+"""
+Created on Tue Jul 22 00:47:05 2014
+
+@author: alina, zzhang
+"""
+
+import time
+import socket
+import select
+import sys
+import string
+import indexer
+import json
+import pickle as pkl
+from chat_utils import *
+import chat_group as grp
+import os
+from cryptography.fernet import Fernet
+
+class Server:
+    def __init__(self):
+        self.new_clients = []  # list of new sockets of which the user id is not known
+        self.logged_name2sock = {}  # dictionary mapping username to socket
+        self.logged_sock2name = {}  # dict mapping socket to user name
+        self.all_sockets = []
+        self.group = grp.Group()
+        # start server
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(SERVER)
+        self.server.listen(5)
+        self.all_sockets.append(self.server)
+        # initialize past chat indices
+        self.indices = {}
+        # sonnet
+        self.sonnet = indexer.PIndex("AllSonnets.txt")
+        self.key = ''
+
+    def new_client(self, sock):
+        # add to all sockets and to new clients
+        print('new client...')
+        sock.setblocking(0)
+        self.new_clients.append(sock)
+        self.all_sockets.append(sock)
+
+    def login(self, sock):
+        # read the msg that should have login code plus username
+        try:
+            msg = json.loads(myrecv(sock))
+            if len(msg) > 0:
+
+                if msg["action"] == "login":
+                    name = msg["name"]
+                    if self.group.is_member(name) != True:
+                        # move socket from new clients list to logged clients
+                        self.new_clients.remove(sock)
+                        # add into the name to sock mapping
+                        self.logged_name2sock[name] = sock
+                        self.logged_sock2name[sock] = name
+                        # load chat history of that user
+                        if name not in self.indices.keys():
+                            try:
+                                self.indices[name] = pkl.load(
+                                    open(name + '.idx', 'rb'))
+                            except IOError:  # chat index does not exist, then create one
+                                self.indices[name] = indexer.Index(name)
+                        print(name + ' logged in')
+                        self.group.join(name)
+                        self.key = Fernet.generate_key()
+                        self.group.store_key(name,self.key)
+                        print( ' done storing key')
+                        mysend(sock, json.dumps(
+                            {"action": "login", "status": "ok","key": self.key.decode('utf-8')}))
+                        print(' done sending key ')
+                     #   self.key = ''
+                    else:  # a client under this name has already logged in
+                        mysend(sock, json.dumps(
+                            {"action": "login", "status": "duplicate"}))
+                        print(name + ' duplicate login attempt')
+                else:
+                    print('wrong code received')
+            else:  # client died unexpectedly
+                self.logout(sock)
+        except:
+            self.all_sockets.remove(sock)
+
+    def logout(self, sock):
+        # remove sock from all lists
+        name = self.logged_sock2name[sock]
+        pkl.dump(self.indices[name], open(name + '.idx', 'wb'))
+        del self.indices[name]
+        del self.logged_name2sock[name]
+        del self.logged_sock2name[sock]
+        self.all_sockets.remove(sock)
+        self.group.leave(name)
+        sock.close()
+
+# ==============================================================================
+# main command switchboard
+# ==============================================================================
+    def handle_msg(self, from_sock):
+        # read msg code
+        msg = myrecv(from_sock)
+     #   self.key = Fernet.generate_key()
+      #  mysend(to_sock, json,dumps({"action":"encrypt","key":self.key}))
+        
+        if len(msg) > 0:
+            # ==============================================================================
+            # handle connect request this is implemented for you
+            # ==============================================================================
+            msg = json.loads(msg)
+            if msg["action"] == "connect":
+                to_name = msg["target"]
+                from_name = self.logged_sock2name[from_sock]
+                if to_name == from_name:
+                    msg = json.dumps({"action": "connect", "status": "self"})
+                # connect to the peer
+                elif self.group.is_member(to_name):
+                    to_sock = self.logged_name2sock[to_name]
+                    self.group.connect(from_name, to_name)
+                    the_guys = self.group.list_me(from_name)
+                    msg = json.dumps(
+                        {"action": "connect", "status": "success"})
+                    for g in the_guys[1:]:
+                        to_sock = self.logged_name2sock[g]
+                        mysend(to_sock, json.dumps(
+                            {"action": "connect", "status": "request", "from": from_name}))
+                else:
+                    msg = json.dumps(
+                        {"action": "connect", "status": "no-user"})
+                mysend(from_sock, msg)
+# ==============================================================================
+# handle messeage exchange: IMPLEMENT THIS
+# ==============================================================================
+            elif msg["action"] == "exchange":
+                from_name = self.logged_sock2name[from_sock]
+                
+                """
+                Finding the list of people to send to and index message
+                """
+                # IMPLEMENTATION
+                # ---- start your code ---- #
+                mesag = msg["message"]
+                self.indices[from_name].add_msg_and_index(mesag)
+                f = Fernet(self.group.get_key(from_name))
+                mesag_decrypted = f.decrypt(mesag.encode('utf-8'))
+
+                # ---- end of your code --- #
+
+                the_guys = self.group.list_me(from_name)[1:]
+              #  print(the_guys)
+                for g in the_guys:
+                    f = Fernet(self.group.get_key(g))
+                    mesag_encrypted = f.encrypt(mesag_decrypted)
+                    to_sock = self.logged_name2sock[g]
+                   
+
+                    # IMPLEMENTATION
+                    # ---- start your code ---- #
+                    
+                    mysend( to_sock, json.dumps(
+                        {"action": "exchange", "msg": mesag_encrypted.decode('utf-8'), "from":from_name}))
+                del mesag_decrypted
+
+                    # ---- end of your code --- #
+
+# ==============================================================================
+# the "from" guy has had enough (talking to "to")!
+# ==============================================================================
+            elif msg["action"] == "disconnect":
+                from_name = self.logged_sock2name[from_sock]
+                the_guys = self.group.list_me(from_name)
+                self.group.disconnect(from_name)
+                the_guys.remove(from_name)
+                if len(the_guys) == 1:  # only one left
+                    g = the_guys.pop()
+                    to_sock = self.logged_name2sock[g]
+                    mysend(to_sock, json.dumps(
+                        {"action": "disconnect", "msg": "everyone left, you are alone"}))
+# ==============================================================================
+#                 listing available peers: IMPLEMENT THIS
+# ==============================================================================
+            elif msg["action"] == "list":
+
+                # IMPLEMENTATION
+                # ---- start your code ---- #
+                from_name = self.logged_sock2name[from_sock]
+                msg = self.group.list_all(from_name)
+                
+              #  msg = "...needs to use self.group functions to work"
+
+                # ---- end of your code --- #
+                mysend(from_sock, json.dumps(
+                    {"action": "list", "results": msg}))
+# ==============================================================================
+#             retrieve a sonnet : IMPLEMENT THIS
+# ==============================================================================
+            elif msg["action"] == "poem":
+
+                # IMPLEMENTATION
+                # ---- start your code ---- #
+                pass
+                target_num = int(msg["target"])
+               # sonnets = indexer.PIndex("AllSonnets.txt")
+                tempList = self.sonnet.get_poem(target_num)
+                poem = ""
+                for i in range(len(tempList)):
+                    poem += tempList[i]
+                
+                print('here:\n', poem)
+
+                # ---- end of your code --- #
+
+                mysend(from_sock, json.dumps(
+                    {"action": "poem", "results": poem}))
+# ==============================================================================
+#                 time
+# ==============================================================================
+            elif msg["action"] == "time":
+                ctime = time.strftime('%d.%m.%y,%H:%M', time.localtime())
+                mysend(from_sock, json.dumps(
+                    {"action": "time", "results": ctime}))
+# ==============================================================================
+#                 search: : IMPLEMENT THIS
+# ==============================================================================
+            elif msg["action"] == "search":
+
+                # IMPLEMENTATION
+                # ---- start your code ---- #
+                userName = self.logged_sock2name(from_sock)
+                f = Fernet(self.group.get_key(userName))
+                del userName
+                search_rslt = ''
+                target_txt = msg["target"]
+                target_txt_decrypted = f.decrypt(target_txt.decode('utf-8'))
+                for User,inx in self.indices.items():
+                    search_rslt += User+":\n"
+                    for line in inx.search(target_txt_decrypted):
+                        search_rslt += str(line[0]) + ': ' + line [1] + "\n"
+                    
+                    print(search_rslt)
+                
+                #search_rslt = "needs to use self.indices search to work"
+                print('server side search: ' + search_rslt)
+                search_rslt_encrypted = f.encrypt(search_rslt.encode('utf-8'))
+                
+                # ---- end of your code --- #
+                mysend(from_sock, json.dumps(
+                    {"action": "search", "results": search_rslt_encrypted.decode('utf-8')}))
+                
+            elif msg["action"] == "game":
+                os.system('gameTrialOne.py') #unknown usability
+
+# ==============================================================================
+#                 the "from" guy really, really has had enough
+# ==============================================================================
+
+        else:
+            # client died unexpectedly
+            self.logout(from_sock)
+
+# ==============================================================================
+# main loop, loops *forever*
+# ==============================================================================
+    def run(self):
+        print('starting server...')
+        while(1):
+            read, write, error = select.select(self.all_sockets, [], [])
+            print('checking logged clients..')
+            for logc in list(self.logged_name2sock.values()):
+                if logc in read:
+                    self.handle_msg(logc)
+            print('checking new clients..')
+            for newc in self.new_clients[:]:
+                if newc in read:
+                    self.login(newc)
+            print('checking for new connections..')
+            if self.server in read:
+                # new client request
+                sock, address = self.server.accept()
+                self.new_client(sock)
+
+
+def main():
+    server = Server()
+    server.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ gameTrialOne.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  6 09:04:25 2018
+
+@author: chenq
+"""
+import time
+import pygame
+from pygame.locals import *
+
+windowSize = (1280,720);
+ 
+class Player:
+    x = 500
+    y = 300
+    Ydirections = {'up': -1, 'down': 1, 'none':0}
+    Xdirections = {'left': -1, 'right':1, 'none':0}
+    XrunningDirection = 'right'
+    YrunningDirection = 'none'
+    speed = 1
+    rec = pygame.Surface((50,50))
+    rec.fill((255,255,255))
+    rect = rec.get_rect()
+ 
+    def running(self):
+        XnextStep = self.x + self.Xdirections[self.XrunningDirection] * self.speed
+        YnextStep = self.y + self.Ydirections[self.YrunningDirection] * self.speed
+        if (XnextStep >= 0 and XnextStep <= windowSize[0] - 50 and YnextStep >= 0 and YnextStep <= windowSize[1] -50):
+            self.x = XnextStep
+            self.y = YnextStep
+        else:
+            pass
+        '''
+        if (self.x >= 0 and self. x <= windowSize[0] - 50 and self.y >= 0 and self.y <= windowSize[1] - 50):
+            if(self.runningDirection == 'right' or self.runningDirection == 'left'):
+                self.x = self.x + self.directions[self.runningDirection] * self.speed
+            else:
+                self.y = self.y + self.directions[self.runningDirection] * self.speed
+        else:
+            if (self.x < 0 or self.x > windowSize[0] - 50):
+                self.x = self.x - self.directions[self.runningDirection] * self.speed
+            if (self.y < 0 or self.y > windowSize[1] - 50):
+                self.y - self.directions[self.runningDirection] * self.speed
+            '''
+    
+    def changeDirection(self,code):
+        if (code == 1):
+            self.YrunningDirection = 'up'
+            self.XrunningDirection = 'none'
+        elif (code == 2):
+            self.YrunningDirection = 'down'
+            self.XrunningDirection = 'none'
+        elif (code == 3):
+            self.XrunningDirection = 'left'
+            self.YrunningDirection = 'none'
+        elif (code == 4):
+            self.XrunningDirection = 'right'
+            self.YrunningDirection = 'none'
+    
+    def moveRight(self):
+        self.x = self.x + self.speed
+ 
+    def moveLeft(self):
+        self.x = self.x - self.speed
+ 
+    def moveUp(self):
+        self.y = self.y - self.speed
+ 
+    def moveDown(self):
+        self.y = self.y + self.speed
+        
+class Snake:
+    x = 200
+    y = 300
+    Ydirections = {'up': -1, 'down': 1, 'none':0}
+    Xdirections = {'left': -1, 'right':1, 'none':0}
+    XrunningDirection = 'right'
+    YrunningDirection = 'none'
+    speed = 2
+    rec = pygame.Surface((50,50))
+    rec.fill((255,0,0))
+    rect = rec.get_rect()
+    t = time.time()
+ 
+    def running(self):
+        XnextStep = self.x + self.Xdirections[self.XrunningDirection] * self.speed
+        YnextStep = self.y + self.Ydirections[self.YrunningDirection] * self.speed
+        if (XnextStep >= 0 and XnextStep <= windowSize[0] - 50 and YnextStep >= 0 and YnextStep <= windowSize[1] -50):
+            self.x = XnextStep
+            self.y = YnextStep
+        else:
+            pass
+        if (self.speed >= 0.5):
+            self.speed -= (time.time()-self.t)/10000;
+    
+    def changeDirection(self,code):
+        if (code == 1):
+            self.YrunningDirection = 'up'
+            self.XrunningDirection = 'none'
+        elif (code == 2):
+            self.YrunningDirection = 'down'
+            self.XrunningDirection = 'none'
+        elif (code == 3):
+            self.XrunningDirection = 'left'
+            self.YrunningDirection = 'none'
+        elif (code == 4):
+            self.XrunningDirection = 'right'
+            self.YrunningDirection = 'none'
+    
+    def moveRight(self):
+        self.x = self.x + self.speed
+ 
+    def moveLeft(self):
+        self.x = self.x - self.speed
+ 
+    def moveUp(self):
+        self.y = self.y - self.speed
+ 
+    def moveDown(self):
+        self.y = self.y + self.speed
+
+class App:
+ 
+    windowWidth = 800
+    windowHeight = 600
+    player = 0
+ 
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.player_surf = None
+        self.snake_surf = None
+        self.player = Player() 
+        self.snake = Snake()
+ 
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(windowSize, pygame.HWSURFACE)
+ 
+        pygame.display.set_caption('Snake game test')
+        self._running = True
+   #     self._image_surf = pygame.image.load("pygame.png").convert()
+        self.player_surf = self.player.rec
+        self.snake_surf = self.snake.rec
+ 
+    def on_event(self, event):
+        if event.type == QUIT:
+            self._running = False
+ 
+    def on_loop(self):
+        pass
+ 
+    def on_render(self):
+        self._display_surf.fill((0,0,0))
+        self._display_surf.blit(self.player_surf,(self.player.x,self.player.y))
+        self._display_surf.blit(self.snake_surf,(self.snake.x,self.snake.y))
+        pygame.display.flip()
+ 
+    def on_cleanup(self):
+        pygame.quit()
+ 
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+ 
+        while( self._running ):
+            pygame.event.pump()
+            keys = pygame.key.get_pressed() 
+            self.player.running()
+            self.snake.running()
+ 
+            if (keys[K_RIGHT]):
+                #self.player.moveRight()
+                self.player.changeDirection(4)
+ 
+            if (keys[K_LEFT]):
+              #  self.player.moveLeft()
+                self.player.changeDirection(3)
+ 
+            if (keys[K_UP]):
+              #  self.player.moveUp()
+                self.player.changeDirection(1)
+ 
+            if (keys[K_DOWN]):
+               # self.player.moveDown()
+                self.player.changeDirection(2)
+            
+            if (keys[K_d]):
+                self.snake.changeDirection(4)
+                
+            if (keys[K_a]):
+              #  self.player.moveLeft()
+                self.snake.changeDirection(3)
+ 
+            if (keys[K_w]):
+              #  self.player.moveUp()
+                self.snake.changeDirection(1)
+ 
+            if (keys[K_s]):
+               # self.player.moveDown()
+                self.snake.changeDirection(2)
+ 
+            if (keys[K_ESCAPE]):
+                self._running = False
+ 
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+ 
+if __name__ == "__main__" :
+    theApp = App()
+    theApp.on_execute()
